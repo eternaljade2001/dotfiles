790d4c1d3e276b23f92df01cebc8a01b5d08999e
eternaljade2001 <etchickadee@tutamail.com>
2024-06-11 23:14:08 -0500

    first actual commit

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+# polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+killall -q polybar
+
+# Launch
+polybar -r
+echo "Bars launched..."
